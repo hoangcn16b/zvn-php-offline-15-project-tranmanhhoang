c555bbdccbd11d08cc6659bc82b60ac46817acd9
TranManhHoang <hoangcn16b@gmail.com>
2022-07-09 12:07:27 +0700

    hoang-cmt

--- define.php
+++ define.php
@@ -2,14 +2,14 @@
 	
 	// ====================== PATHS ===========================
 	define ('DS'					, '/');
-	define ('ROOT_PATH'				, dirname(__FILE__));						// Định nghĩa đường dẫn đến thư mục gốc
-	define ('LIBRARY_PATH'			, ROOT_PATH . DS . 'libs' . DS);			// Định nghĩa đường dẫn đến thư mục thư viện
-	define ('LIBRARY_EXT_PATH'		, LIBRARY_PATH . DS . 'extends' . DS);		// Định nghĩa đường dẫn đến thư mục thư viện
-	define ('PUBLIC_PATH'			, ROOT_PATH . DS . 'public' . DS);			// Định nghĩa đường dẫn đến thư mục public							
-	define ('UPLOAD_PATH'			, PUBLIC_PATH . 'files' . DS);			// Định nghĩa đường dẫn đến thư mục thư thư viện
-	define ('APPLICATION_PATH'		, ROOT_PATH . DS . 'application' . DS);		// Định nghĩa đường dẫn đến thư mục public							
-	define ('TEMPLATE_PATH'			, PUBLIC_PATH . 'template' . DS);		// Định nghĩa đường dẫn đến thư mục public							
-	define ('SCRIPT_PATH'			, PUBLIC_PATH  . 'scripts' . DS);		// Định nghĩa đường dẫn đến thư  
+	define ('ROOT_PATH'				, dirname(__FILE__));						// đến thư mục gốc
+	define ('LIBRARY_PATH'			, ROOT_PATH . DS . 'libs' . DS);			// đến thư mục thư viện
+	define ('LIBRARY_EXT_PATH'		, LIBRARY_PATH . DS . 'extends' . DS);		// đến thư mục thư viện
+	define ('PUBLIC_PATH'			, ROOT_PATH . DS . 'public' . DS);			// đến thư mục public							
+	define ('UPLOAD_PATH'			, PUBLIC_PATH . 'files' . DS);			// đến thư mục thư thư viện
+	define ('APPLICATION_PATH'		, ROOT_PATH . DS . 'application' . DS);		// đến thư mục public							
+	define ('TEMPLATE_PATH'			, PUBLIC_PATH . 'template' . DS);		// đến thư mục public							
+	define ('SCRIPT_PATH'			, PUBLIC_PATH  . 'scripts' . DS);		// đến thư  
 	
 
 	define	('ROOT_URL'				, DS);
@@ -22,10 +22,10 @@
 	define	('DEFAULT_ACTION'		, 'index');
 
 	// img URL
-	define ('UPLOAD_URL'			, 'public' . DS . 'files' . DS);			// Định nghĩa đường dẫn đến thư mục thư thư viện
+	define ('UPLOAD_URL'			, 'public' . DS . 'files' . DS);		// đến thư mục thư thư viện
 
 	//ckEditor
-	define ('CKEDITOR_PATH'			, 'public' . DS . 'ckeditor' . DS);		// Định nghĩa đường dẫn đến thư  
+	define ('CKEDITOR_PATH'			, 'public' . DS . 'ckeditor' . DS);		// đến thư mục thư viện
 	//services layout
 	define	('SERVICE_LAYOUT'			, TEMPLATE_URL . 'frontend' . DS . 'html' . DS );
 
